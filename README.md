4de9b84468cf6d99ade118eb3194dd8fdb3fa988
Yu_olgun <yu_olgun@hotmail.com>
2018-04-09 14:43:58 +0200

    - Project Lombok to provide @Value @Data @Slf4j

---
+++ README.md
@@ -0,0 +1,12 @@
+A starting template, examples and so on
+
+- Spring Boot 2
+- Lombok
+  * Enable Annotation Processing. At IntelliJ -> "Settings > Build > Compiler > Annotation Processors"
+  * Download Lombok plugin for auto-completion at IntelliJ
+  * Provides @Value, @Data, @Slf4j
+
+# Build
+```
+./mvnw spring-boot:run -pl echo
+```
--- echo/src/main/java/com/yolgun/echo/controller/Echo.java
+++ echo/src/main/java/com/yolgun/echo/controller/Echo.java
@@ -1,13 +1,16 @@
 package com.yolgun.echo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class Echo {
   @GetMapping("/{in}")
   public static String echo(@PathVariable String in) {
+    log.info("Received {}", in);
     return in;
   }
 }
